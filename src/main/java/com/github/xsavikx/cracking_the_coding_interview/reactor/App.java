4448c8e679f1a680bf0d750d5f6a853ed30d791b
Iurii Sergiichuk <i.sergiichuk@samsung.com>
2014-08-26 13:53:22 +0300

    Initial commit

---
+++ src/main/java/com/github/xsavikx/cracking_the_coding_interview/reactor/App.java
@@ -0,0 +1,12 @@
+package com.github.xsavikx.cracking_the_coding_interview.reactor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out
+				.println("This is reactor project for Cracking the coding interview!");
+	}
+}
