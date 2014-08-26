105b7d5e4455f419a5dbcf022225d6ba3309d7bf
Iurii Sergiichuk <savik.ne@gmail.com>
2014-08-26 17:28:56 +0300

    Arrays and Strings
    task 1.6 done.

---
+++ arrays_and_strings/src/main/java/com/github/xsavikx/cracking_the_coding_interview/arrays_and_strings/ImageRotator.java
@@ -0,0 +1,67 @@
+package com.github.xsavikx.cracking_the_coding_interview.arrays_and_strings;
+
+/**
+ * Class for Cracking the coding Interview task 1.6
+ * 
+ * Description of task(in <b>Russian</b>): Дано: изображение в виде матрицы
+ * размером N x N, где каждый пиксель занимает 4 байта. Напишите метод,
+ * поворачивающий изображение на 90°.
+ * 
+ * @author Iurii Sergiichuk
+ *
+ */
+public class ImageRotator {
+
+	public static enum Direction {
+		CLOCKWISE, COUNTERCLOCKWISE;
+	}
+
+	/**
+	 * Makes given image matrix to given direction 90-degree rotation
+	 * 
+	 * @param imageMatrix
+	 *            matrix to rotate
+	 * @return rotated to given direction matrix
+	 */
+	public static int[][] rotateImage(int[][] imageMatrix, Direction direction) {
+		if (imageMatrix == null || direction == null) {
+			throw new IllegalArgumentException(
+					"imageMatrix and direction cannot be null");
+		}
+		int[][] rotatedImageMatrix = new int[imageMatrix[0].length][];
+		for (int i = 0; i < rotatedImageMatrix.length; i++) {
+			rotatedImageMatrix[i] = new int[imageMatrix.length];
+		}
+		switch (direction) {
+		case CLOCKWISE:
+			for (int i = 0; i < imageMatrix.length; i++) {
+				for (int j = 0; j < imageMatrix[i].length; j++) {
+					rotatedImageMatrix[j][imageMatrix.length - 1 - i] = imageMatrix[i][j];
+				}
+			}
+			break;
+		case COUNTERCLOCKWISE:
+			for (int i = 0; i < imageMatrix.length; i++) {
+				for (int j = 0; j < imageMatrix[i].length; j++) {
+					rotatedImageMatrix[j][i] = imageMatrix[i][j];
+				}
+			}
+			break;
+		default:
+			throw new IllegalArgumentException("No given direction was found");
+
+		}
+
+		return rotatedImageMatrix;
+	}
+
+	public static void printImageMatrix(int[][] imageMatrix) {
+		for (int i = 0; i < imageMatrix.length; i++) {
+			for (int j = 0; j < imageMatrix[i].length; j++) {
+				System.out.print(imageMatrix[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
---
+++ arrays_and_strings/src/test/java/com/github/xsavikx/cracking_the_coding_interview/arrays_and_strings/ImageRotatorTest.java
@@ -0,0 +1,43 @@
+package com.github.xsavikx.cracking_the_coding_interview.arrays_and_strings;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.github.xsavikx.cracking_the_coding_interview.arrays_and_strings.ImageRotator.Direction;
+
+public class ImageRotatorTest {
+
+	@Test
+	public void testRotateImageClockwise() {
+		int[][] imageMatrix = { { 1, 1, 1, 1 }, { 2, 2, 2, 2 }, { 3, 3, 3, 3 },
+				{ 4, 4, 4, 4 }, { 5, 5, 5, 5 } };
+		int[][] imageMatrixRotatedClockwise = { { 5, 4, 3, 2, 1 },
+				{ 5, 4, 3, 2, 1 }, { 5, 4, 3, 2, 1 }, { 5, 4, 3, 2, 1 } };
+		assertArrayEquals(imageMatrixRotatedClockwise,
+				ImageRotator.rotateImage(imageMatrix, Direction.CLOCKWISE));
+	}
+
+	@Test
+	public void testRotateImageCounterclockwise() {
+		int[][] imageMatrix = { { 1, 1, 1, 1 }, { 2, 2, 2, 2 }, { 3, 3, 3, 3 },
+				{ 4, 4, 4, 4 }, { 5, 5, 5, 5 } };
+		int[][] imageMatrixRotatedClockwise = { { 1, 2, 3, 4, 5 },
+				{ 1, 2, 3, 4, 5 }, { 1, 2, 3, 4, 5 }, { 1, 2, 3, 4, 5 } };
+		assertArrayEquals(imageMatrixRotatedClockwise,
+				ImageRotator.rotateImage(imageMatrix,
+						Direction.COUNTERCLOCKWISE));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testRotateImageIllegalArgumentExceptionImageNull() {
+		ImageRotator.rotateImage(null, Direction.CLOCKWISE);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testRotateImageIllegalArgumentExceptionDirectionNull() {
+		int[][] imageMatrix = { { 1, 1, 1, 1 }, { 2, 2, 2, 2 }, { 3, 3, 3, 3 },
+				{ 4, 4, 4, 4 }, { 5, 5, 5, 5 } };
+		ImageRotator.rotateImage(imageMatrix, null);
+	}
+}
